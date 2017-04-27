df272abf097f4af111e04846a2f6b6e5dd1e3d51
Victor Reyes <victor.reyes@sietevoz.com>
2017-04-27 11:23:26 +0200

    vuelta a recuperar los controladores, marcha atras correcta

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -1,3 +1,14 @@
+<?php
+define('BASEPATH') OR exit('NO DIRECT SCRIPT ACCESS ALLOWED');
+
+class Login extends CI_Controller {
+
+	public function index ()//pruebas y pruebas, borrar este comentario 
+	{
+		$this->load->view('login');
+	}
+
+}
 
 
 
