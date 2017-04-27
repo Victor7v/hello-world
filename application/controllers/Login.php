0aeccf7269cf34b92a81a64c74350650850de6a7
Victor Reyes <victor.reyes@sietevoz.com>
2017-04-27 11:20:26 +0200

    deshaciendo controladores

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -1,14 +1,3 @@
-<?php
-define('BASEPATH') OR exit('NO DIRECT SCRIPT ACCESS ALLOWED');
-
-class Login extends CI_Controller {
-
-	public function index ()//pruebas y pruebas, borrar este comentario 
-	{
-		$this->load->view('login');
-	}
-
-}
 
 
 
